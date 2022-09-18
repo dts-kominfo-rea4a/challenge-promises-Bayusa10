8de28c57e52121b84ef038871b1db3f40fdef5e2
Bayusa10 <bayusa69@gmail.com>
2022-09-18 17:17:15 +0700

    Tambah logic fungsi yang ada di index.js

--- promise.js
+++ promise.js
@@ -1,7 +1,28 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion)=>{
+  let dataFilmTheaterIXX  = await promiseTheaterIXX(); //ambil array promise promiseTheaterIXX
+  let dataFilmTheaterVGC  = await promiseTheaterVGC(); //ambil array promise promiseTheaterVGC
+
+  return new Promise((resolve, reject)=>{ 
+      if(emotion === null){ // jika parameter emosi tidak berisi
+        reject('Emosi tidak boleh kosong!'); //kembalian fungsi reject
+      } else {
+        // hitung panjang array object dengan isian sesuai parameter
+        var lenghOfFilterDataIXX = dataFilmTheaterIXX.
+                                    filter(len => len.hasil.toLowerCase() === emotion.toLowerCase()).length;
+        
+        var lengthOfFilterDataVGC= dataFilmTheaterVGC.
+                                    filter(len => len.hasil.toLowerCase() === emotion.toLowerCase()).length;
+
+        //jumlahkan kedua array object yang sudah diambil panjang datanya
+        var sumLength            = lenghOfFilterDataIXX + lengthOfFilterDataVGC;
+
+        resolve(sumLength);
+      }
+  });
+};
 
 module.exports = {
   promiseOutput,
